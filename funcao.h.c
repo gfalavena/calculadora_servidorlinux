cb211c1aad6bdefb77e3a365b1449a5b527f8c97
Gabriel Falavena <gabriel.oliveira1370@etec.sp.gov.br>
2023-10-18 18:01:34 -0300

    primeiro commit funcao

---
+++ funcao.h.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+float soma(float soma1, float soma2){
+	float totalSoma;
+	
+	totalSoma= soma1 + soma2;
+	return totalSoma;
+}
+
+float subtracao(float sub1, float sub2){
+	float totalSubtracao;
+
+	totalSubtracao= sub1 - sub2;
+	return totalSubtracao;
+}
+
+float multiplicacao(float mult1, float mult2){
+	float totalMultiplicacao;
+	
+	totalMultiplicacao= mult1 * mult2;
+	return totalMultiplicacao;
+}
+
+float divisao(float div1, float div2){
+	float totalDivisao;
+	
+	totalDivisao= div1/div2;
+	return totalDivisao;
+}
+
